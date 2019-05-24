a5fa08c99982f652e1d32c26ea7d0b77b440c4b0
Rodri <rodrigoiniestacicuendez@gmail.com>
2019-05-24 20:05:25 +0200

    Initial commit

---
+++ app/src/main/java/net/proyecto/pruebasproyecto/viewmodels/adapters/PagerAdapter.kt
@@ -0,0 +1,31 @@
+package net.proyecto.pruebasproyecto.viewmodels.adapters
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import net.proyecto.pruebasproyecto.views.EntrenamientosFragment
+import net.proyecto.pruebasproyecto.views.NubeFragment
+import net.proyecto.pruebasproyecto.views.RutinasFragment
+
+class PagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm)
+{
+    override fun getItem(position : Int): Fragment {
+        return  when (position){
+            0 -> { RutinasFragment() }
+            1 -> { EntrenamientosFragment() }
+            else -> { NubeFragment() }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return  when (position){
+            0 -> "Rutinas"
+            1 -> "Entrenamientos"
+            else -> "Nube de Entrenamientos"
+        }
+    }
+}
